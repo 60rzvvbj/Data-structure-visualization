838b9996b38d4442855c15567c30fba658094d89
60rzvvbj <1808078515@qq.com>
2022-05-04 13:20:36 +0800

    feat: init Project Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="homeRoot">
-    <div class="show"></div>
-    <div class="control"></div>
-  </div>
+	<div class="homeRoot">
+		<a-button @click="toProject">Project</a-button>
+	</div>
 </template>
 
 <script>
-// import HelloWorld from "@/components/HelloWorld.vue";
+import { useRouter } from 'vue-router';
 
 export default {
-  name: "Home",
-  components: {},
-  setup() {},
+	name: "Home",
+	components: {},
+	setup() {
+		let router = useRouter();
+		function toProject() {
+			router.push('/project');
+		}
+		return {
+			toProject
+		};
+	},
 };
 </script>
 
 <style scoped>
 .homeRoot {
-  display: flex;
-  height: 100%;
+	display: flex;
+	height: 100%;
 }
 
 .show {
-  width: 60%;
-  background-color: pink;
+	width: 60%;
+	background-color: pink;
 }
 
 .control {
-  flex: 1;
-  background-color: skyblue;
+	flex: 1;
+	background-color: skyblue;
 }
 </style>
---
+++ src/views/project/Project.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		{{ msg }}
+	</div>
+</template>
+
+<script lang="js">
+
+export default {
+	name: 'Project',
+	setup() {
+		let msg = 'aaa';
+		return {
+			msg
+		};
+	}
+};
+</script>
+
+<style scoped>
+</style>
